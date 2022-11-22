e1eb825541fd0e879e2ffd1b55461ddbf29161bc
양재혁 <107400911+Akkapda@users.noreply.github.com>
2022-11-22 22:03:41 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int n = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            switch (str.charAt(i)) {
+                case 'A': case 'B': case 'C':
+                    n += 3;
+                    break;
+                case 'D': case 'E': case 'F':
+                    n += 4;
+                    break;
+                case 'G': case 'H': case 'I':
+                    n += 5;
+                    break;
+                case 'J': case 'K': case 'L':
+                    n += 6;
+                    break;
+                case 'M': case 'N': case 'O':
+                    n += 7;
+                    break;
+                case 'P': case 'Q': case 'R': case 'S':
+                    n += 8;
+                    break;
+                case 'T': case 'U': case 'V':
+                    n += 9;
+                    break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    n += 10;
+                    break;
+            }
+        }
+        System.out.println(n);
+        br.close();
+    }
+}
