1973d5044474bb7b092d3114a325cb40f5de4f4c
양재혁 <107400911+Akkapda@users.noreply.github.com>
2024-05-27 11:41:07 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 23720 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 public class Main {
@@ -8,26 +6,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int k = Integer.parseInt(br.readLine());
+
         Stack<Integer> stack = new Stack<>();
-        int num, sum = 0;
-
+        int answer = 0;
         for(int i = 0; i < k; i++) {
-            num = Integer.parseInt(br.readLine());
-            if (num == 0) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
+            int temp = Integer.parseInt(br.readLine());
+            if (temp == 0 && !stack.isEmpty()) {
+                answer -= stack.pop();
+            } else {
+                stack.push(temp);
+                answer += temp;
             }
-            else stack.push(num);
-        }
-        
-        int n = stack.size();
-
-        for(int i = 0; i < n; i++) {
-            sum += stack.pop();
         }
 
-        System.out.println(sum);
-        br.close();
+        System.out.println(answer);
     }
 }
