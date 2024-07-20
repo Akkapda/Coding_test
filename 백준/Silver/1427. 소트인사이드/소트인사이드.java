4c8173c097acad84223dee0836c955f04a5cdfdb
양재혁 <107400911+Akkapda@users.noreply.github.com>
2024-07-20 09:31:15 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int[] array = new int[str.length()];
+
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            array[i] = Integer.parseInt(c + "");
+        }
+
+        Arrays.sort(array);
+
+        for(int i = array.length - 1; i > -1; i--) {
+            bw.write(array[i] + "");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
